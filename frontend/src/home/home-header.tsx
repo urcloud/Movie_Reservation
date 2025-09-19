0203caf8bc174620c6e574bb3eee34865bd3f0f9
Daeki Yoon <dowellware@gmail.com>
2025-09-19 11:52:54 +0900

    home header pagelayout

---
+++ frontend/src/home/home-header.tsx
@@ -0,0 +1,9 @@
+import { cx } from '../commons/cx';
+
+export const HomeHeader = () => {
+  return (
+    <div className={cx('h-8', 'p-2', 'bg-primary', 'flex items-center')}>
+      HomeHeader
+    </div>
+  );
+};
--- frontend/src/home/home.tsx
+++ frontend/src/home/home.tsx
@@ -1,7 +1,11 @@
+import { PageLayout } from '../layouts/page-layout';
+import { HomeHeader } from './home-header';
+
 export const Home = () => {
   return (
-    <div className='text-2xl font-semibold text-center mt-4'>
+    <PageLayout>
+      <HomeHeader />
       산업수학 2 프로젝트에 오신걸 환영합니다.
-    </div>
+    </PageLayout>
   );
 };
