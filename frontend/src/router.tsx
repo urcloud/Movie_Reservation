d0fcd24e897213e5e76bc10b6ef16efa3d5e2f9e
Daeki Yoon <dowellware@gmail.com>
2025-09-19 11:08:20 +0900

    front layout

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,16 @@
 import { Route, Router, Switch } from 'wouter';
 import { Home } from './home/home';
+import { AppLayout } from './layouts/app-layout';
 export const AppRouter = () => {
   return (
     <Router>
-      <Switch>
-        <Route path={'/'}>
-          <Home />
-        </Route>
-      </Switch>
+      <AppLayout>
+        <Switch>
+          <Route path={'/'}>
+            <Home />
+          </Route>
+        </Switch>
+      </AppLayout>
     </Router>
   );
 };
