4db1c952022fc0ce9855cf5178374231cc716a37
minseo <slkoms@naver.com>
2025-10-02 01:08:27 +0900

    0925

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,9 @@
 import { Route, Router, Switch } from 'wouter';
 import { Home } from './home/home';
 import { AppLayout } from './layouts/app-layout';
+import { GuestBooking } from './home/guest-booking';
+import { BookingDetail } from './home/booking-detail';
+
 export const AppRouter = () => {
   return (
     <Router>
@@ -8,6 +11,12 @@ export const AppRouter = () => {
         <Switch>
           <Route path={'/'}>
             <Home />
+          </Route>
+           <Route path={'/guest-booking'}>
+            <GuestBooking />
+          </Route>
+                  <Route path={'/guest-booking/:id'}>
+            {params => <BookingDetail id={params.id} />}
           </Route>
         </Switch>
       </AppLayout>
