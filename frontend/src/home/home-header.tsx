198b71f0140289638684d35196bb6d75b55682aa
Han, Gyumin <han35799@naver.com>
2025-10-02 14:26:44 +0900

    home header

--- frontend/src/home/home-header.tsx
+++ frontend/src/home/home-header.tsx
@@ -1,20 +1,23 @@
-import { cx } from '../commons/cx';
+import { Link } from 'wouter';
 
 export const HomeHeader = () => {
   return (
-    <div
-      className={cx(
-        'sticky',
-        'top-0',
-        'left-0',
-        'right-0',
-        'h-8',
-        'p-2',
-        'bg-primary',
-        'flex items-center',
-      )}
-    >
-      HomeHeader
-    </div>
+    <header className="flex absolute top-5 right-10">
+          <Link to="/signup">
+          <button
+            className="text-blue-600 hover:underline"
+            >
+            회원가입
+          </button>
+          </Link>
+         
+          <Link to="/login">
+          <button
+          className="text-blue-600 hover:underline"
+          >
+            로그인
+          </button>
+          </Link>
+      </header>
   );
 };
