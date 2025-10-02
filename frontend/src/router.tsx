a9ce8345567696949a33a32dd4d1fffb474db0e1
Kimmingyu <very5happy@naver.com>
2025-10-02 12:37:11 +0900

    temp

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,8 @@
 import { Route, Router, Switch } from 'wouter';
 import { Home } from './home/home';
-<<<<<<< HEAD
-import { MovieEdit } from './home/movie-edit';
-import { MovieIndex } from './home/movie-index';
-import { MovieRegist } from './home/movie-inform';
+import { MovieEdit } from './movieinfo/movie-edit';
+import { MovieIndex } from './movieinfo/movie-index';
+import { MovieRegist } from './movieinfo/movie-inform';
 
 export const AppRouter = () => {
   return (
@@ -12,29 +11,16 @@ export const AppRouter = () => {
         <Route path={'/'}>
           <Home />
         </Route>
-        <Route path={'/home/movie-edit'}>
+        <Route path={'/movie-edit'}>
           <MovieEdit />
         </Route>
-        <Route path={'/home/movie-index'}>
+        <Route path={'/movie-index'}>
           <MovieIndex />
         </Route>
-        <Route path={'/home/movie-regist'}>
+        <Route path={'/movie-regist'}>
           <MovieRegist />
         </Route>
       </Switch>
-=======
-import { AppLayout } from './layouts/app-layout';
-export const AppRouter = () => {
-  return (
-    <Router>
-      <AppLayout>
-        <Switch>
-          <Route path={'/'}>
-            <Home />
-          </Route>
-        </Switch>
-      </AppLayout>
->>>>>>> main
     </Router>
   );
 };
