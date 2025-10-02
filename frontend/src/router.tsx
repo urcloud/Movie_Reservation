b489edc5c00886655ee88fc12ff61f49d18bd501
Han, Gyumin <han35799@naver.com>
2025-10-02 14:21:25 +0900

    로그인 회원가입 화면

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,14 +1,20 @@
 import { Route, Router, Switch } from 'wouter';
 import { Home } from './home/home';
 import { AppLayout } from './layouts/app-layout';
+import { LoginForm } from './pages/login';
+import { SignupForm } from './pages/signup';
+import { HomeHeader } from './home/home-header';
 export const AppRouter = () => {
   return (
     <Router>
       <AppLayout>
         <Switch>
-          <Route path={'/'}>
-            <Home />
-          </Route>
+          <div className='bg white'>
+          <HomeHeader/>
+          <Route path={'/'} component={Home}/>
+          <Route path={'/login'} component={LoginForm}/>
+          <Route path={'/signup'} component={SignupForm}/>
+          </div>
         </Switch>
       </AppLayout>
     </Router>
