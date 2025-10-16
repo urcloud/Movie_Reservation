9daa1916b6871007addd14c81955fa603fc29fd9
Daeki Yoon <dowellware@gmail.com>
2025-10-16 13:30:49 +0900

    theater edit list modified

--- frontend/src/theaters/theater-edit.tsx
+++ frontend/src/theaters/theater-edit.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import { Button } from '../commons/button';
 import { Input } from '../commons/input';
 
 export const TheaterEdit = () => {
+  console.log('history.state', history.state);
+  const [theater, setTheater] = useState(history.state);
+  // const { id, name, rows, cols, seats } = history.state;
+  console.log('theater', theater);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    console.log('theater after submit', [...formData]);
+    history.back();
+  };
   return (
     <div>
       <Button
@@ -12,24 +24,28 @@ export const TheaterEdit = () => {
         이전화면
       </Button>
 
-      <form className='flex flex-col mt-20 gap-4 p-4 max-w-md mx-auto'>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col mt-20 gap-4 p-4 max-w-md mx-auto'
+      >
         <Input
           type='text'
+          name='name'
           className='border border-gray-300'
-          placeholder='상영관 이름 수정'
+          defaultValue={theater.name}
         />
         <Input
           type='number'
+          name='cols'
           className='border border-gray-300'
-          placeholder='좌석 열 수정'
+          defaultValue={theater.cols}
         />
         <Input
           type='number'
+          name='rows'
           className='border border-gray-300'
-          placeholder='좌석 행 수정'
+          defaultValue={theater.rows}
         />
-      
-    
 
         <Button type='submit' className='bg-gray-500 p-2 rounded'>
           수정
--- frontend/src/theaters/theaters-list.tsx
+++ frontend/src/theaters/theaters-list.tsx
@@ -1,13 +1,20 @@
 import { Link } from 'wouter';
 import { Button } from '../commons/button';
-
+import { useState } from 'react';
 
 const theaterData = [
-  { id: 1, name: '1관', seats: 150 },
-  { id: 2, name: '2관',  seats: 120 },
+  { id: 1, name: '1관', rows: 30, cols: 5, seats: 150 },
+  { id: 2, name: '2관', rows: 30, cols: 4, seats: 120 },
 ];
 
 export const TheaterList = () => {
+  const [theaters, setTheaters] = useState(theaterData);
+
+  const handleDelete = (id: number) => {
+    const modified = theaters.filter((theater) => theater.id !== id);
+    setTheaters(modified);
+    console.log('delete', id);
+  };
   return (
     <div>
       <Button
@@ -27,10 +34,9 @@ export const TheaterList = () => {
             상영관 등록
           </Button>
         </Link>
-
       </form>
 
-      {theaterData.map((theater) => (
+      {theaters.map((theater) => (
         <div
           key={theater.id}
           className='flex justify-evenly w-full h-20 mt-0.5 bg-gray-300 border border-black'
@@ -38,12 +44,12 @@ export const TheaterList = () => {
           <span className='flex items-center justify-center w-1/4'>
             {theater.name}
           </span>
-      
+
           <span className='flex items-center justify-center w-1/4'>
             {theater.seats}석
           </span>
           <span className='flex gap-1'>
-           <Link to={`/${theater.id}`}>
+            <Link to={`/${theater.id}`}>
               <Button
                 type='button'
                 className='bg-green-500 w-20 h-20 p-2 rounded'
@@ -51,7 +57,7 @@ export const TheaterList = () => {
                 조회
               </Button>
             </Link>
-            <Link to={`/edit/${theater.id}`}>
+            <Link to={`/edit/${theater.id}`} state={theater}>
               <Button
                 type='button'
                 className='bg-blue-500 w-20 h-20 p-2 rounded'
@@ -59,7 +65,11 @@ export const TheaterList = () => {
                 수정
               </Button>
             </Link>
-            <Button type='button' className='bg-gray-500 w-20 h-20 p-2 rounded'>
+            <Button
+              type='button'
+              onClick={() => handleDelete(theater.id)}
+              className='bg-gray-500 w-20 h-20 p-2 rounded'
+            >
               삭제
             </Button>
           </span>
