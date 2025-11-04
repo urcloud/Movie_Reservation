f747cb8f03d98282bd114202f149528663a5cf7a
Kimmingyu <very5happy@naver.com>
2025-11-04 11:24:19 +0900

    page addition

--- frontend/src/movies/movie-register.tsx
+++ frontend/src/movies/movie-register.tsx
@@ -2,69 +2,96 @@ import { Button } from '../commons/button';
 import { Input } from '../commons/input';
 
 export const MovieRegister = () => {
-      const GoBack = () => {
+  const GoBack = () => {
     window.history.back();
-  }
+  };
 
-  const handleClick = () => {alert('등록되었습니다.');
+  const handleClick = () => {
+    alert('등록되었습니다.');
     window.location.href = '/movies';
-  }
+  };
   return (
-    <div>
+    <div className='relative pt-20'>
       <Button
         type='button'
-        className='absolute top-2 right-2 bg-gray-500 p-2 rounded' onClick={GoBack}>
+        className='absolute top-2 right-2 bg-white text-gray-700 border border-gray-300 p-2 rounded hover:bg-gray-50'
+        onClick={GoBack}
+      >
         이전화면
       </Button>
-      <form className='flex flex-col mt-20 gap-4 p-4 max-w-md mx-auto'>
+
+      <form className='flex flex-col gap-4 p-4 max-w-md mx-auto'>
         <Input
           type='text'
           className='border border-gray-300'
           placeholder='영화명'
+          required
         />
         <Input
           type='text'
           className='border border-gray-300'
           placeholder='감독명'
+          required
         />
         <Input
           type='text'
           className='border border-gray-300'
           placeholder='주연배우'
+          required
         />
         <Input
           type='date'
           className='border border-gray-300'
           placeholder='개봉일'
+          required
         />
         <Input
           type='date'
           className='border border-gray-300'
           placeholder='상영 종료일'
+          required
         />
         <Input
           type='number'
           className='border border-gray-300'
           placeholder='러닝타임'
+          required
         />
         <Input
           type='number'
           className='border border-gray-300'
           placeholder='관람등급'
+          required
         />
         <Input
           type='text'
           className='border border-gray-300'
           placeholder='장르'
+          required
         />
-        <Input
-          type='text'
-          className='border border-gray-300'
+        <textarea
+          className='border border-gray-300 h-64'
           placeholder='영화 소개'
         />
-        <h1 className='mt-4'>포스터 이미지 업로드</h1>{' '}
-        <Input type='file' className='border border-gray-300' />
-        <Button type='submit' className='bg-gray-500 p-2 rounded' onClick={handleClick}>
+        <div className='flex justify-between items-center mt-4'>
+          <h1 className='text-xl font-bold'>포스터 이미지 업로드</h1>
+          <label
+            htmlFor='poster-upload'
+            className='bg-gray-200 text-sm text-gray-700 px-3 py-1 rounded border border-gray-400 cursor-pointer hover:bg-gray-300'
+          >
+            파일 찾아보기
+          </label>
+        </div>
+        <Input
+          type='file'
+          id='poster-upload'
+          className='hidden'
+        />
+        <Button
+          type='button'
+          className='bg-white text-gray-700 border border-gray-300 p-2 rounded hover:bg-gray-50'
+          onClick={handleClick}
+        >
           등록
         </Button>
       </form>
