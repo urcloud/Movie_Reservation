6ff04e6428c64ee19806e85e9806dc4455491f9d
Han, Gyumin <han35799@naver.com>
2025-11-13 12:53:28 +0900

    server ctrl

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 import { errorHandler } from './helpers/error-helper';
 import router from './route';
 
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use(cookieParser());
 app.get('/api/hello', (req, res) => {
   res.send('Hello. Welcome! Here is in Node.js');
 });
